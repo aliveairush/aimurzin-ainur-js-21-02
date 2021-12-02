bed6a210b850c9ad1b25de69d3782d11a06f91ca
Aimurzin Ainur <aliveairush@gmail.com>
2021-12-02 21:53:45 +0300

    HW 17.  Minor commit. Добавил сохранение статуса темы в localstorage и при загрузке страницы пользователя отображается "Идет загрузка"

--- hw_15_dummyapi_and_hoc/my-dummy-app/src/forms/userProfile/UserForm.tsx
+++ hw_15_dummyapi_and_hoc/my-dummy-app/src/forms/userProfile/UserForm.tsx
@@ -12,44 +12,49 @@ const UserForm = () => {
   useScrollToTop();
   const { id } = useParams() as Params;
   const [user, setUser] = useState({} as UserProfileType);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    getUserById(id, setUser, ({ error }: ResponseError) => alert(error), () => {});
+    getUserById(id, setUser, ({ error }: ResponseError) => alert(error), () => { setLoading(false); });
   }, []);
 
   return (
     <div className="containter">
-      <div className="user-profile-info">
-        <div className="user-profile-info__image">
-          <img src={user.picture} alt={user.firstName} />
+      {loading ? 'Идет загрузка' : (
+        <div className="user-profile-info">
+
+          <div className="user-profile-info__image">
+            <img src={user.picture} alt={user.firstName} />
+          </div>
+          <div className="user-profile-info__name">
+            <span>{`${user.lastName} ${user.firstName}`}</span>
+          </div>
+          <div className="user-profile-info__other">
+            <h3>Additional info:</h3>
+            <p>
+              <strong>Email:&nbsp;</strong>
+              {user.email}
+            </p>
+            <p>
+              <strong>Phone:&nbsp;</strong>
+              {user.phone}
+            </p>
+            <p>
+              <strong>Date of birth:&nbsp;</strong>
+              {user.dateOfBirth}
+            </p>
+            <p>
+              <strong>Register date:&nbsp;</strong>
+              {user.registerDate}
+            </p>
+            <p>
+              <strong>Gender:&nbsp;</strong>
+              {user.gender}
+            </p>
+          </div>
         </div>
-        <div className="user-profile-info__name">
-          <span>{`${user.lastName} ${user.firstName}`}</span>
-        </div>
-        <div className="user-profile-info__other">
-          <h3>Additional info:</h3>
-          <p>
-            <strong>Email:&nbsp;</strong>
-            {user.email}
-          </p>
-          <p>
-            <strong>Phone:&nbsp;</strong>
-            {user.phone}
-          </p>
-          <p>
-            <strong>Date of birth:&nbsp;</strong>
-            {user.dateOfBirth}
-          </p>
-          <p>
-            <strong>Register date:&nbsp;</strong>
-            {user.registerDate}
-          </p>
-          <p>
-            <strong>Gender:&nbsp;</strong>
-            {user.gender}
-          </p>
-        </div>
-      </div>
+      )}
+
     </div>
   );
 };
