3dd88b1e37734ea4c7cc4c46d123923eefa2d316
Aimurzin Ainur <aliveairush@gmail.com>
2021-12-03 01:56:23 +0300

    HW 17.  Выполнил опциональную задачу по Пагинации.

--- hw_15_dummyapi_and_hoc/my-dummy-app/src/contexts/ThemeContext.tsx
+++ hw_15_dummyapi_and_hoc/my-dummy-app/src/contexts/ThemeContext.tsx
@@ -29,34 +29,5 @@ const ThemeContextProvider = ({ children }: Props) => {
     </ThemeContext.Provider>
   );
 };
-//
-// class ThemeContextProvider extends React.Component<Props, State> {
-//   constructor(props: any) {
-//     super(props);
-//     const prevTheme = localStorage.getItem('theme') || 'light';
-//     this.state = {
-//       theme: prevTheme,
-//     };
-//
-//     this.toggleTheme = this.toggleTheme.bind(this);
-//   }
-//
-//   toggleTheme = () => {
-//     const newTheme = this.state.theme === 'light' ? 'dark' : 'light';
-//     localStorage.setItem('theme', newTheme);
-//     this.setState({ theme: newTheme });
-//   };
-//
-//   render() {
-//     const { children } = this.props;
-//     return (
-//       <ThemeContext.Provider
-//         value={{ theme: this.state.theme, toggleTheme: this.toggleTheme }}
-//       >
-//         {children}
-//       </ThemeContext.Provider>
-//     );
-//   }
-// }
 
 export default ThemeContextProvider;
